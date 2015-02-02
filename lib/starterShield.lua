bab550933130313c99654b39fcc2b294d23c9441
Priyanka Nigam <pnigam@berkeley.edu>
2015-02-01 16:44:05 -0800

    Removed print statements from LED blinking function

--- lib/starterShield.lua
+++ lib/starterShield.lua
@@ -65,13 +65,11 @@ function blinker(color)
    local state = 0
    return function ()
       if state  == 1 then 
-	 print ("blink on", state)
-	 shield.LED.on(color)
+          shield.LED.on(color)
       else 
-	 print ("blink off", state)
-	 shield.LED.off(color)
+          shield.LED.off(color)
       end
-      state=1-state
+          state=1-state
    end
 end
 
