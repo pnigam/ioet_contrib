4fd2cb843e11ea8569ab43cd23c4c5f7e2dfa1fa
Priyanka Nigam <pnigam@berkeley.edu>
2015-02-03 21:43:57 -0800

    Initial i2c write implementation. Not tested yet.

--- lib/i2creg.lua
+++ lib/i2creg.lua
@@ -15,10 +15,19 @@ function REG:r(reg)
     -- create array with address
     -- write address
     -- read register with RSTART
+    -- storm.i2c.read()
     -- check all return values
+    
 end
 
 function REG:w(reg, value)
+    local extra = 0
+    if value % 8 > 0 then extra = 1 end 
+    count = 1 + value / 8 + extra
+    arr = storm.array.create(count, storm.array.UINT8)
+    arr:set(1, reg)
+    for i=2,count do arr:set(i, value >> 8)
+    storm.i2c.write(storm.i2c.INT + address, storm.i2c.START, arr, function(status) print (status) end)
     -- TODO:
     -- create array with address and value
     -- write
